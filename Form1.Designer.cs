6c0121b9c2102960b4f8ed669a2ff02d029b7364
Kevin Fabricio Morocho Almeida <fabrialmeida1796@gmail.com>
2022-02-01 11:42:27 -0500

    Add this.Backcolo in designer.cs

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,6 +29,7 @@ partial class Form1
     /// </summary>
     private void InitializeComponent()
     {
+        this.BackColor=Color.BlueViolet;
         this.components = new System.ComponentModel.Container();
         this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
         this.ClientSize = new System.Drawing.Size(800, 450);
